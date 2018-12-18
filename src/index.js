6d40154b9e966df762a8434373eef061747d1e03
nguyen tien thanh <thanh.uou@gmail.com>
2018-12-18 14:52:14 +0900

    IE support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import "@babel/polyfill";
 import indexRoutes from './routes/index';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
